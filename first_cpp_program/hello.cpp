7b1101380932d376c7894c2ee3200fccbf0c6639
IamJamesRooke <me@jamesrooke.blog>
2025-02-21 14:59:56 -0500

    did my first C++ program, a bitch to setup the compiler

---
+++ first_cpp_program/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "Count: " << i << std::endl;
+    }
+    
+    return 0;
+}
